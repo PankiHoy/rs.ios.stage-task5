16ed827f2615f112ae3abf296aed9ccbc5d2a148
Maksim Karaseu <dassive_mick@mail.ru>
2021-06-12 23:31:33 +0300

    feat: implementation ex1(dumbass solution n^2), ex2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,27 @@
 import Foundation
 
 class StockMaximize {
-
+//    [1, 7, 3, 5]
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var profit = 0
+        var max = 0
+        
+        if prices.count > 0 {
+            for i in 1..<prices.count {
+                if prices[i] >= prices[i-1] {
+                    for j in i..<prices.count {
+                        if prices[j] > prices[i] {
+                            max = prices[j]
+                        } else {
+                            max = prices[i]
+                        }
+                    }
+                    profit += max - prices[i-1]
+                }
+            }
+        }
+
+        return profit
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,38 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var counts = [String]() // ["21"]
+        var tempCount = ""
+        var repeatString = [String]() //[]
+        var result = "" //
+        
+//        21[a8[c]]
+        
+        for char in message {
+            if char.isNumber {
+                tempCount = "\(tempCount)\(char)"
+            } else if char == "[" {
+                if tempCount == "" {
+                    counts.append("1")
+                } else {
+                    counts.append(tempCount)
+                }
+                tempCount = ""
+                repeatString.append(result)
+                result = ""
+            } else if char == "]" {
+                var tempString = ""
+                if let counts = Int(counts.popLast() ?? "1") {
+                    for _ in 0..<counts {
+                        tempString += result // "ccccccc"
+                    }
+                }
+                result = repeatString.popLast()! + tempString // "acccccccc"
+            } else if char.isLetter {
+                result += String(char) // "c"
+            }
+        }
+        
+        return result
     }
 }
